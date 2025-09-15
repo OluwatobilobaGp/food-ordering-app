59d7152c8c90733f3674ea87a5d813c03c699116
Oluwatobiloba Asaolu <asaoluoluwatobijoseph@gmail.com>
2025-09-15 21:58:43 +0100

    Search Tab done

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,5 +1,9 @@
+import CartButton from '@/components/CartButton';
+import MenuCard from '@/components/MenuCard';
 import { getCategories, getMenu } from '@/lib/appwrite';
 import useAppwrite from '@/lib/useAppwrite';
+import { MenuItem } from '@/type';
+import cn from 'clsx';
 import { useLocalSearchParams } from 'expo-router';
 import { useEffect } from 'react';
 import { FlatList, Text, View } from 'react-native';
@@ -8,7 +12,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 const Search = () => {
   const { category, query } = useLocalSearchParams<{query: string; category: string}>()
   
-  const { data, refetch, loading } = useAppwrite({ fn: getMenu, params: { category, query, limit: 6,}});
+  const { data, refetch, loading } = useAppwrite({ fn: getMenu, params: { category, query, limit: 6}});
   const { data: categories} = useAppwrite({ fn: getCategories });
 
 useEffect(() => {
@@ -22,16 +26,36 @@ useEffect(() => {
       <FlatList 
           data={data} 
           renderItem={({item, index}) => {
+            const isFirstRightColItem = index % 2 === 0;
               return (
-                <View className='flex-1 max-w[48%]'>
-                  <Text>Menu Card</Text>
-
+                <View className={cn('flex-1 max-w[48%]', isFirstRightColItem ? 'mt-10' : 'mt-0')}>                  
+                  <MenuCard item={item as MenuItem} />
                 </View>
               )
           }} 
+          keyExtractor={item => item.$id}
+          numColumns={2}
+          columnWrapperClassName='gap-7'
+          contentContainerClassName='gap-7 px-5 pb-32'
+          ListHeaderComponent={() => (
+            <View className='my-5 gap-5' >
+              <View className='flex-between flex-row w-full'>
+                <View className='flex-start'>
+                  <Text className='small-bold uppercase text-primary' style={{ fontFamily: 'QuickSand-Bold' }}>Search</Text>
+                  <View>
+                    <Text className='text-base font-quicksand-bold text-dark-100' style={{ fontFamily: 'QuickSand-Bold' }}>Find Your Favorite Food</Text>
+                  </View>
+                </View>
+
+                <CartButton />
+              </View>
+              <Text>Search Input</Text>
+              <Text>Filter</Text>
+            </View>
+          )}
+          ListEmptyComponent={() => !loading && <Text>No Result</Text>}
       />
 
-      {/* <Button title='seed' onPress={() => seed().catch((error) => console.log("failed to seed database", error))} /> */}
     </SafeAreaView>
   )
 }
