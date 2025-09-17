ccbf44992e832fb37c222f530fb261cd9a499ba5
Oluwatobiloba Asaolu <asaoluoluwatobijoseph@gmail.com>
2025-09-18 00:21:06 +0100

    Search BAr add

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,18 @@
-import { Text, View } from 'react-native'
+import { useLocalSearchParams } from 'expo-router'
+import { useState } from 'react'
+import { TextInput, View } from 'react-native'
 
 const SearchBar = () => {
+  const params = useLocalSearchParams<{ query?: string }>()
+  const [query, setQuery] = useState(params.query);
+
   return (
-    <View>
-      <Text>SearchBar</Text>
+    <View className='searchbar'>
+      <TextInput
+          className='flex-1 p-5'
+          placeholder='search for pizzaz, burges...'
+
+       />
     </View>
   )
 }
