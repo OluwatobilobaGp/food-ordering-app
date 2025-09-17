1bbe208f2910957efa0c63ab7aeb0a1ef99c9873
Oluwatobiloba Asaolu <asaoluoluwatobijoseph@gmail.com>
2025-09-18 00:34:44 +0100

    Search BAr

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,7 @@
+import { images } from '@/constants'
 import { useLocalSearchParams } from 'expo-router'
 import { useState } from 'react'
-import { TextInput, View } from 'react-native'
+import { Image, TextInput, TouchableOpacity, View } from 'react-native'
 
 const SearchBar = () => {
   const params = useLocalSearchParams<{ query?: string }>()
@@ -11,8 +12,18 @@ const SearchBar = () => {
       <TextInput
           className='flex-1 p-5'
           placeholder='search for pizzaz, burges...'
-
+          value={query}
+          placeholderTextColor="A0A0A0"
        />
+       <TouchableOpacity className='pr-5' onPress={() => console.log("Search pressed")}>
+          <Image
+              source={images.search}
+              className='size-6'
+              resizeMode='contain'
+              tintColor="#5D5F6D"
+
+          />
+       </TouchableOpacity>
     </View>
   )
 }
