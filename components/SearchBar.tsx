f83badb00972e5d786b98dd77d5864a7e003001a
Oluwatobiloba Asaolu <asaoluoluwatobijoseph@gmail.com>
2025-09-18 01:03:27 +0100

    Implemented Search and filter

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,5 +1,5 @@
 import { images } from '@/constants'
-import { useLocalSearchParams } from 'expo-router'
+import { router, useLocalSearchParams } from 'expo-router'
 import { useState } from 'react'
 import { Image, TextInput, TouchableOpacity, View } from 'react-native'
 
@@ -7,23 +7,38 @@ const SearchBar = () => {
   const params = useLocalSearchParams<{ query?: string }>()
   const [query, setQuery] = useState(params.query);
 
+  const handleSearch = (text: string) => {
+    setQuery(text);
+
+    if (!text) router.setParams({ query: undefined });
+  };
+
+  const handleSubmit = () => {
+        if(query.trim()) router.setParams({ query });
+  }
+
   return (
     <View className='searchbar'>
       <TextInput
-          className='flex-1 p-5'
-          placeholder='search for pizzaz, burges...'
-          value={query}
-          placeholderTextColor="A0A0A0"
-       />
-       <TouchableOpacity className='pr-5' onPress={() => console.log("Search pressed")}>
-          <Image
-              source={images.search}
-              className='size-6'
-              resizeMode='contain'
-              tintColor="#5D5F6D"
-
-          />
-       </TouchableOpacity>
+        className='flex-1 p-5'
+        placeholder='search for pizzaz, burges...'
+        value={query}
+        onChangeText={handleSearch}
+        onSubmitEditing={handleSubmit}
+        placeholderTextColor="A0A0A0"
+        returnKeyType='Search'
+      />
+      <TouchableOpacity
+        className='pr-5'
+        onPress={() => router.setParams({ query })}
+      >
+        <Image
+          source={images.search}
+          className='size-6'
+          resizeMode='contain'
+          tintColor="#5D5F6D"
+        />
+      </TouchableOpacity>
     </View>
   )
 }
