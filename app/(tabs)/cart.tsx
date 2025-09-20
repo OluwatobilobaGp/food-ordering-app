eddc20080a30ac8b3b0ce4f46cd822039682b27f
Oluwatobiloba Asaolu <asaoluoluwatobijoseph@gmail.com>
2025-09-20 18:22:22 +0100

    Cart added

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,3 +1,4 @@
+import CartItem from '@/components/CartItem';
 import CustomButton from '@/components/CustomButton';
 import CustomHeader from '@/components/CustomHeader';
 import { useCartStore } from '@/store/cart.store';
@@ -28,7 +29,7 @@ const Cart = () => {
     <SafeAreaView className='bg-white h-full'>
       <FlatList
         data={items}
-        renderItem={({ item }) => <Text>Cart Item</Text>}
+        renderItem={({ item }) => <CartItem item={item} />}
         keyExtractor={(item) => item.id}
         contentContainerClassName='pd-28 px-5 pt-5'
         ListHeaderComponent={() => <CustomHeader title='Your Cart' />}
--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -1,11 +1,15 @@
 import { images } from '@/constants';
+import { useCartStore } from '@/store/cart.store';
+import { router } from 'expo-router';
 import React from 'react';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 
 const CartButton = () => {
-    const totalItems = 10;
+  const {getTotalItems} = useCartStore();
+  const totalItems = getTotalItems();
+
   return (
-    <TouchableOpacity className="flex items-center justify-center size-10 rounded-full bg-dark-100" onPress={() => { }}>
+    <TouchableOpacity className="flex items-center justify-center size-10 rounded-full bg-dark-100" onPress={() => router.push('/cart')}>
       <Image source={images.bag} className="size-5" resizeMode="contain" />
       {totalItems > 0 && (
         <View className='absolute -top-2 -right-1 flex items-center justify-center size-5 bg-primary rounded-full font-quicksand-bold' >
